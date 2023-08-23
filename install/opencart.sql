eaf5c4b7a57ca95786eb75cfc11715daf7930da2
Pascal Laroche <16697148+plaroche1978@users.noreply.github.com>
2023-08-22 21:02:24 -0400

    Change title name

--- install/opencart.sql
+++ install/opencart.sql
@@ -3346,7 +3346,7 @@ INSERT INTO `oc_setting` (`store_id`, `code`, `key`, `value`, `serialized`) VALU
 (0, 'config', 'config_ftp_username', '', 0),
 (0, 'config', 'config_ftp_port', '21', 0),
 (0, 'config', 'config_ftp_hostname', '', 0),
-(0, 'config', 'config_meta_title', 'Cloud Automation Services', 0),
+(0, 'config', 'config_meta_title', 'ACME Corp Demo', 0),
 (0, 'config', 'config_meta_description', 'My Store', 0),
 (0, 'config', 'config_meta_keyword', '', 0),
 (0, 'config', 'config_theme', 'default', 0),
